42de613a3c6bb99bd1f548f8965a49e596cc91a2
Maaz Ahmad <maaz.c10@gmail.com>
2017-07-14 07:33:53 -0400

    WPC generation is now functional, easier to codegen and prevents bugs

--- bin/Test.java
+++ bin/Test.java
@@ -23,29 +23,9 @@ public class Sum {
 	public static int sumList(List<Integer> data) {
 		int sum = 0;
 		sum = 0;
-		{
-			int i = 0;
-			i = 0;
-			boolean loop$0 = false;
-			loop$0 = false;
-			while (true) {
-				if (loop$0) {
-					{
-						int flat$1 = i + 1;
-						i = (int) flat$1;
-					}
-					;
-				}
-				int flat$2 = data.size();
-				loop$0 = i < flat$2;
-				if (loop$0) {
-					Integer flat$3 = data.get(i);
-					int flat$4 = sum + flat$3;
-					sum = (int) flat$4;
-				} else {
-					break;
-				}
-			}
+		for (Integer val : data) {
+			int flat$0 = sum + val;
+			sum = (int) flat$0;
 		}
 		return sum;
 	}
